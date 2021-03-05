c985fbc2ff82f4a7c906b34d104f766d23815cc4
Jose de la Cruz <31924509+KryptekDragon@users.noreply.github.com>
2021-03-04 19:38:59 -0500

    Add files via upload

--- breakout.js
+++ breakout.js
@@ -50,12 +50,12 @@ document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
 
 
-function mouseMoveHandler(e) {
-    var relativeX = e.clientX - canvas.offsetLeft;
+/*function mouseMoveHandler(e) {
+    var relativeX = e.clientX - (canvas.offsetLeft);
     if(relativeX > 0 && relativeX < canvas.width) {
         paddleX = relativeX - paddleWidth/2;
     }
-}
+}*/
 
 function drawBall() {
     ctx.beginPath();
